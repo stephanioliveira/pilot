e333ab84bc182966175b74f800b1db7c0e6c8577
Derek Stavis <dekestavis@gmail.com>
2017-12-05 13:19:51 -0200

    themes: add switch theming

--- webapp/stories/Switch/index.js
+++ webapp/stories/Switch/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 
 import { storiesOf } from '@storybook/react'
+import { ThemeProvider } from 'react-css-themr'
 
 import Switch from '../../src/components/Switch'
+import defaultTheme from '../../src/components/Switch/defaultTheme/index.css'
+import highContrastTheme from '../../src/components/Switch/highContrastTheme/index.css'
 
 import style from './style.css'
 
@@ -35,20 +38,42 @@ SwitchState.defaultProps = {
 storiesOf('Switch', module)
   .add('All styles', () => (
     <div className={style.container}>
-      <section>
-        <h2>Enabled</h2>
-        <p>Checked</p>
-        <SwitchState checked />
-        <p>Unchecked</p>
-        <SwitchState checked={false} />
-      </section>
-      <section>
-        <h2>Disabled</h2>
-        <p>Checked</p>
-        <SwitchState disabled checked />
-        <p>Unchecked</p>
-        <SwitchState disabled checked={false} />
-      </section>
+      <ThemeProvider theme={{ PLSwitch: defaultTheme }}>
+        <div>
+          <section>
+            <h2>Enabled</h2>
+            <p>Checked</p>
+            <SwitchState checked />
+            <p>Unchecked</p>
+            <SwitchState checked={false} />
+          </section>
+          <section>
+            <h2>Disabled</h2>
+            <p>Checked</p>
+            <SwitchState disabled checked />
+            <p>Unchecked</p>
+            <SwitchState disabled checked={false} />
+          </section>
+        </div>
+      </ThemeProvider>
+      <ThemeProvider theme={{ PLSwitch: highContrastTheme }}>
+        <div>
+          <section>
+            <h2>Enabled</h2>
+            <p>Checked</p>
+            <SwitchState checked />
+            <p>Unchecked</p>
+            <SwitchState checked={false} />
+          </section>
+          <section>
+            <h2>Disabled</h2>
+            <p>Checked</p>
+            <SwitchState disabled checked />
+            <p>Unchecked</p>
+            <SwitchState disabled checked={false} />
+          </section>
+        </div>
+      </ThemeProvider>
     </div>
   ))
 

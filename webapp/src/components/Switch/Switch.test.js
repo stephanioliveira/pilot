4f35f995c5953bd0fa9c354c521ac1ec68520f47
Gabriel Godoy <gabriel.oliveira@stone.com.br>
2017-12-27 12:32:17 -0200

    switch: add strings prop for i18n

--- webapp/src/components/Switch/Switch.test.js
+++ webapp/src/components/Switch/Switch.test.js
@@ -6,14 +6,51 @@ import Switch from './index'
 describe('Switch', () => {
   const onChange = jest.fn()
 
-  const component = shallow(
-    <Switch
-      onChange={onChange}
-    />
-  )
-
   it('should trigger onChange', () => {
+    const component = shallow(
+      <Switch
+        onChange={onChange}
+      />
+    )
     component.find('input').simulate('change')
     expect(onChange).toHaveBeenCalled()
+  })
+
+  it('Should render on default translation', () => {
+    const component = shallow(
+      <Switch
+        checked
+        onChange={onChange}
+      />
+    )
+
+    expect(component.find('span').text()).toBe('on')
+  })
+
+  it('Should render on label in portuguese', () => {
+    const component = shallow(
+      <Switch
+        checked
+        onChange={onChange}
+        strings={{
+          on: 'ligado',
+        }}
+      />
+    )
+
+    expect(component.find('span').text()).toBe('ligado')
+  })
+
+  it('Should render off label in portuguese', () => {
+    const component = shallow(
+      <Switch
+        onChange={onChange}
+        strings={{
+          off: 'desligado',
+        }}
+      />
+    )
+
+    expect(component.find('span').text()).toBe('desligado')
   })
 })

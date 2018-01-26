2f4d2bbcd6e78f73a6ac2dbed78485f6c7fe2649
Derek Stavis <dekestavis@gmail.com>
2018-01-26 01:06:46 -0200

    chore: fix lint by locally-disabling

--- packages/react-scripts/template/src/registerServiceWorker.js
+++ packages/react-scripts/template/src/registerServiceWorker.js
@@ -1,3 +1,6 @@
+/* eslint-disable */
+// Disable eslint to keep compatibility with react-scripts
+
 // In production, we register a service worker to serve assets from local cache.
 
 // This lets the app load faster on subsequent visits in production, and gives
